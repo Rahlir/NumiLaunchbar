a8f6c610e4f72c88c8ca03581688e952835f06fa
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2018-09-10 12:46:40 -0400

    Version alpha 0.1
    First fully functioning version with simple features. Select the action
    in launchbar and press <space> to enter input. Numi must be opened.

--- Numi Calculate.lbaction/Contents/Scripts/default.js
+++ Numi Calculate.lbaction/Contents/Scripts/default.js
@@ -4,18 +4,57 @@
 // Default script for the LaunchBar action
 
 const port = 15055
+const delay = .25
 
 function run(argument) {
-	if (argument == undefined) { // Inform the user that there was no argument 
-		LaunchBar.alert('No argument was passed to the action'); 
-	} 
-	else { // Return a single item that describes the argument 
-		return [{ title: 'An argument was passed: ' + argument }];
-	}
+	var openCode = openNumi(delay);
+	LaunchBar.alert('No argument or wrong argument was passed to the action'); 
 }
 
 function runWithString(string) {
-	var body = 'q=' + string;
-	var result = HTTP.post('http://localhost:'+port, {body: body});
-	LaunchBar.log(result.data);
+	var request = encodeURIComponent(string);
+	var response = HTTP.get('http://localhost:'+port+'/?q='+request);
+
+	var result;
+	if (response.data != undefined) {
+		result = [{
+			title: response.data
+			}, {
+			title: string + ' = ' + response.data,
+			subtitle: 'EXPR'
+			}];
+		}
+	else if (response.error != undefined && response.error.includes('Could not connect')) {
+		var openCode = openNumi(delay);
+		if (openCode == 1) {
+			return runWithString(string);
+		}
+		else if (openCode == 2) {
+			LaunchBar.alert('Could not connect to Numi'
+										  + '\nCheck that Alfred Extension is allowed in Numi preferences');
+		}
+		else {
+			LaunchBar.alert('Could not open Numi.app, try opening it manually');
+		}
+	}
+	else if (response.error != undefined) {
+		LaunchBar.alert('Error: ' + response.error);
+	}
+	else {
+		result = [{
+			title: string
+			}, {
+			title: string + ' = ' + string,
+			subtitle: 'EXPR'
+			}];
+	}
+
+	return result;
+}
+
+function openNumi(delay) {
+	// This function returns 1 on successful opening of Numi,
+	// 2 when Numi is already opened,
+	// and 3 when Numi could not be opened
+	return 3;
 }
