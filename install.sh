bbc7f76769954a5734edcce734b375686fca6a93
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2018-10-30 16:03:15 -0400

    Default version is now optimized for non-setapp Numi, for setapp use -S option

--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+usage="$(basename "$0") [-hS]
+
+where:
+	-h         show this help and exit
+	-S         install for setapp version of Numi"
+
+while getopts ':hS' option; do
+	case "$option" in
+		h) 	echo "$usage"
+				exit
+				;;
+		S)  version="setapp"
+			  ;;
+		\?) printf "illegal option: -%s\n" "$OPTARG" >&2
+			  echo "$usage" >&2
+				exit 1
+				;;
+	esac
+done
+
+
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 
@@ -8,3 +30,7 @@ rm -vr ~/Library/Application\ Support/LaunchBar/Actions/Numi\ Calculate.lbaction
 
 printf "\n${bold}Installing new version:${normal}\n"
 cp -vr Numi\ Calculate.lbaction ~/Library/Application\ Support/LaunchBar/Actions/
+
+if [ "$version" == "setapp" ]; then
+	sed -i '' 's/com.nikolaeu.numi</com.nikolaeu.numi-setapp</g' "$HOME/Library/Application Support/Launchbar/Actions/Numi Calculate.lbaction/Contents/Info.plist"
+fi
